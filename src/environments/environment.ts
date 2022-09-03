7e19162778223f9768513c7334dfaa15516d4d6c
nicovelar <nicovelar@gmail.com>
2022-09-03 20:11:00 -0300

    link

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBase: "http://localhost:8080",
+  apiBase: "https://card-game-marvel.herokuapp.com",
   firebase: {
     apiKey: "AIzaSyBhYMS0ndr4YHnkhZ9RLcxHO_d4oO6XlWA",
   authDomain: "cardgame-92222.firebaseapp.com",
